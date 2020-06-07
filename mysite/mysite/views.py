4c3e4fb8bc1245060055aad4f0d89828a3cd45b0
jekajeka63 <jekajeka63@gmail.com>
2020-06-07 19:31:20 +0300

    Дизайн

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "about.html")
 
 
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'about.html')
