c63bdf4191838226f4035ba830bbd062f5714ed4
jekajeka63 <jekajeka63@gmail.com>
2020-06-10 00:43:44 +0300

    Дизайн

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index , name= 'index' ),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('news/', views.news, name='news'),
 ]
