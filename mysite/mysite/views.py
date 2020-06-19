22c0ff4f5155032e72bccfabf06c71dea4316d6f
jekajeka63 <jekajeka63@gmail.com>
2020-06-19 06:43:12 +0300

    Дизайн

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 def index(request):
     return render(request, "index.html")
@@ -15,4 +17,20 @@ def prices(request):
     return render(request, 'template.html', context=content)
 
 def news(request):
-    return render(request, 'template.html')
+    s = requests.get("http://auto.bigmir.net/autonews")
+    soup = BeautifulSoup(s.content,"html.parser")
+    news_header = []
+    news_body = []
+    list = soup.find_all("li", class_="b-prev-articles__list-item g-clearfix")
+    for item in list:
+        tag = item.find_all("a", class_="b-prev-articles__list-item__title")
+        news_header.append(tag[0].text)
+        tag = item.find_all("div", class_="b-prev-articles__list-item__description")
+        news_body.append(tag[0].text)
+
+    # сделать циклом 10 новостей
+    # вывести в дикт
+
+    news_cont = dict(zip(news_header, news_body))
+    content = {"news": news_cont}
+    return render(request, 'template.html', content)
