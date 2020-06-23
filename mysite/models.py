fc2d51b65681d23ea05f20117f0c92cea2c9812e
jekajeka63 <jekajeka63@gmail.com>
2020-06-24 00:18:20 +0300

    Дизайн

--- mysite/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class services(models.Model):
-    name_servive = models.CharField(max_length=100)
-    price_service = models.CharField(max_length=20)
-
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,8 +18,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index , name= 'index' ),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('news/', views.news, name='news'),
     path('price/', views.prices, name='price'),
+    path('parts/', views.patrs, name='parts'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
