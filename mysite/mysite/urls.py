1e492cebd8b104aa8f52b466a3729bc47950e084
jekajeka63 <jekajeka63@gmail.com>
2020-07-14 20:41:55 +0300

    Дизайн

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -25,3 +25,27 @@ urlpatterns = [
     path('parts/', views.patrs, name='parts'),
     path('contacts/', views.contacts, name='contacts'),
 ]
+from bs4 import BeautifulSoup
+import requests
+s = requests.session()
+
+url = "https://b2b.ad.ua/views/account/login"
+headers = {"Referer": "https://b2b.ad.ua/Account/Login",
+            "Origin": "https://b2b.ad.ua",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15"}
+s.get(url,headers=headers)
+
+firstresp = requests.get(url,headers=headers)
+soup = BeautifulSoup(firstresp.content, "lxml")
+
+token = soup.find("form").find("input").get("value")
+data = {"__RequestVerificationToken" : token, "ComId" : "15", "UserName" : "33969", "Password" : "033969", "RememberMe" : "False"}
+headers = {"Referer": "https://b2b.ad.ua/Account/Login",
+            "Origin": "https://b2b.ad.ua",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15"}
+
+resp = requests.post("https://b2b.ad.ua/views/account/login?__RequestVerificationToken="+token+"&ComId=15&UserName=33969&Password=033969&RememberMe=false")
